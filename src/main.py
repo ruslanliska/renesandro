2d754ba2165de8ea6d1a0cd25df325ac3d28248d
ruslanliska56 <ruslan@talentpath.co>
2023-10-13 12:45:39 +0300

    Created init database and included format router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+from renesandro.src.database.core import engine
+from renesandro.src.database.manage import init_database
+from renesandro.src.formats.router import router as format_router
 
 app = FastAPI()
+app.include_router(format_router)
+init_database(engine)
 
 
 @app.get('/')
