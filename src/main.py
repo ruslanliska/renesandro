7f46931f03a99ede9e9cda3c206107d94a6f6f8c
ruslanliska56 <ruslan@talentpath.co>
2023-10-16 12:39:48 +0300

    Created dynamic base_api_url

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 from fastapi import FastAPI
 from renesandro.src.api import api_router
+from renesandro.src.config import BASE_API_URL
 from renesandro.src.database.core import engine
 from renesandro.src.database.manage import init_database
 
 app = FastAPI(title='Renesandro')
 
-app.include_router(api_router, prefix='/api/v1')
+app.include_router(api_router, prefix=BASE_API_URL)
 
 
 init_database(engine)
