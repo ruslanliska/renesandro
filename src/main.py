70ce5b9cd669162cf2791735804fb22ee204aa99
ruslanliska56 <ruslan@talentpath.co>
2023-10-16 12:38:17 +0300

    Removed routers initialization from main, moved to api, added title for app

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+from renesandro.src.api import api_router
 from renesandro.src.database.core import engine
 from renesandro.src.database.manage import init_database
-from renesandro.src.formats.router import router as format_router
 
-app = FastAPI()
-app.include_router(format_router)
+app = FastAPI(title='Renesandro')
+
+app.include_router(api_router, prefix='/api/v1')
+
+
 init_database(engine)
 
 
