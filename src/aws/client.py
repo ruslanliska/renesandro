7b4e81bf666338c5cc0f1883597cca7d87d65f77
ruslanliska56 <ruslan@talentpath.co>
2023-10-20 11:31:34 +0300

    Modified list_files in S3Client, added functionality to read files in the folder

--- src/aws/client.py
+++ src/aws/client.py
@@ -7,19 +7,60 @@ from renesandro.src.config import AWS_SECRET_ACCESS_KEY
 
 class S3Client:
     def __init__(self, bucket_name):
-        session = Session(
+        _session = Session(
             aws_access_key_id=AWS_ACCESS_KEY,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         )
-        s3 = session.resource('s3')
+        s3 = _session.resource('s3')
         self.bucket = s3.Bucket(bucket_name)
 
-    def list_files(self):
+    def list_files(self, prefix=None):
+        """
+        Lists files in an Amazon S3 bucket.
+
+        Args:
+            prefix (str, optional): A prefix to filter files
+            by folder path. If provided, only files within the
+            specified folder will be listed. The prefix must end with
+            a forward slash ('/').
+
+        Returns:
+            list: A list of file names or full S3
+                  file objects in the specified folder.
+
+        Note:
+            - If `prefix` is provided, only files within
+              the specified folder will be listed.
+            - If no `prefix` is provided, all files
+              in the bucket will be listed.
+
+        Example:
+            To list all files in the root of the bucket:
+            s3_manager.list_files()
+
+            To list files in a specific folder (e.g., 'images/'):
+            s3_manager.list_files(prefix='images/')
+        """
+        files = []
+        if prefix:
+            for object_summary in self.bucket.objects.filter(Prefix=prefix):
+                file_path = object_summary.key.strip()
+                file_name = file_path.split('/')[1]
+                try:
+                    if file_name:
+                        files.append(file_path.split('/')[1])
+                except IndexError:
+                    continue
+            return files
         for s3_file in self.bucket.objects.all():
-            print(s3_file.key)
+            files.append(s3_file)
+        return files
 
     def upload_file(self, file_body, file_name):
         self.bucket.put_object(
             Key=f'{file_name}.jpg',
             Body=open(file_body, 'rb'),
         )
+
+    def get_objects_urls_folder(self, folder_name):
+        ...
