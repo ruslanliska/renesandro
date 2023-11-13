e3a56f4e6c786bfecace18a1365507ba87d2865e
ruslanliska56 <ruslan@talentpath.co>
2023-11-13 08:57:06 +0200

    Create init image route

--- src/images/router.py
+++ src/images/router.py
@@ -1,8 +1,15 @@
 from __future__ import annotations
 
 from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import status
+from renesandro.src.images.schemas import ImageGenerationData
+router = APIRouter()
 
-router = APIRouter(
-    tags=['images'],
-    prefix='/images',
+
+@router.post(
+    '/generate',
+    status_code=status.HTTP_200_OK,
 )
+def generate_images(image_data: ImageGenerationData = Depends()):
+    return {'image_data': image_data}
