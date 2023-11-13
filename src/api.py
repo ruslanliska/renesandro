ed71bb75772d0ff3621d4482a4d625aacb4876c3
ruslanliska56 <ruslan@talentpath.co>
2023-11-13 09:18:58 +0200

    Include text router

--- src/api.py
+++ src/api.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 from fastapi import APIRouter
 from renesandro.src.formats.router import router as formats_router
 from renesandro.src.images.router import router as images_router
-
+from renesandro.src.texts.router import router as texts_router
 api_router = APIRouter()
 
 api_router.include_router(images_router, prefix='/images', tags=['images'])
 api_router.include_router(formats_router, prefix='/formats', tags=['formats'])
+api_router.include_router(texts_router, prefix='/texts', tags=['texts'])
