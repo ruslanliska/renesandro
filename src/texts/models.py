a73205f27da0c53f0d560bb9c7c40c2c28231b40
ruslanliska56 <ruslan@talentpath.co>
2023-11-13 16:15:06 +0200

    Create models for texts

--- src/texts/models.py
+++ src/texts/models.py
@@ -2,14 +2,24 @@ from __future__ import annotations
 
 from renesandro.src.database.core import Base
 from sqlalchemy import Column
+from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
 
 
-class Texts(Base):
-    __tablename__ = 'texts'
+class TextDescription(Base):
+    __tablename__ = 'text_description'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     description = Column(String)
     name = Column(String, nullable=True)
-    texts = Column(String)
+
+    def __repr__(self):
+        return f'<TextDescription(id={self.id}, description={self.description}, name={self.name})>'
+
+
+class Text(Base):
+    __tablename__ = 'text'
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    text = Column(String)
+    description = Column(ForeignKey('text_description.id'))
