8834d5d5a8cfea4cf796713ea0ec2a5e1d4d399c
ruslanliska56 <ruslan@talentpath.co>
2023-11-14 15:51:02 +0200

    Modify TextDescriptionSchema, add validator for name

--- src/texts/schemas.py
+++ src/texts/schemas.py
@@ -1,11 +1,22 @@
 from __future__ import annotations
 
 from pydantic import BaseModel
+from pydantic import field_validator
 
 
 class TextDescriptionSchema(BaseModel):
     description: str
     name: str
+    max_characters: int
+    texts_quantity: int
+    product_description: str
+    text_type: str
+    auditory_description: str
+
+    @field_validator('name')
+    @classmethod
+    def name_validator(cls, value: str):
+        return value.lower()
 
 
 class TextSchema(BaseModel):
