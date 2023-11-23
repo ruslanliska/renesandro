d53da39c9ea589b683f6c518ec2bc95c4d9ffb62
ruslanliska56 <ruslan@talentpath.co>
2023-11-23 15:13:57 +0200

    Create DescriptionAlreadyExists

--- src/texts/exceptions.py
+++ src/texts/exceptions.py
@@ -15,3 +15,11 @@ class DescriptionNotFound(HTTPException):
             status_code=status.HTTP_404_NOT_FOUND,
             detail='Description with such name does not exist.',
         )
+
+
+class DescriptionAlreadyExists(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Description with such name already exists.',
+        )
